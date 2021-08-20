d15fee974b22c8cec5d3a89be4c04e29b68c725a
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-21 00:15:30 +0300

    PassingBetweenForms

---
+++ 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string message;
+        public string bywho;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = message;
+            label2.Text = bywho;
+        }
+    }
+}
