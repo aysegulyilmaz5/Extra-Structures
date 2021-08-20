1150c468c5add20cd8fe10c46eb269787066b85f
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-21 01:27:22 +0300

    Create MathFunctions.cs

---
+++ MathFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(textBox1.Text);
+            // label2.Text = Math.Abs(number).ToString();
+            //  label2.Text = Math.Ceiling(number).ToString();
+            //label2.Text = Math.Floor(number).ToString();
+            //label2.Text = Math.Pow(number, 3).ToString();
+            label2.Text = Math.Sqrt(number).ToString();
+        }
+    }
+}
