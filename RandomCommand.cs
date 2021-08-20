5346a24dbef3eccd3e5c0448e04dda221534e907
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 21:40:16 +0300

    Create RandomCommand.cs

---
+++ RandomCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int num1, num2, num3;
+            num1 = rnd.Next(1, 50);
+            num2 = rnd.Next(1, 100);
+            num3 = rnd.Next(1, 150);
+            label1.Text = num1.ToString();
+            label2.Text = num2.ToString();
+            label3.Text = num3.ToString();
+
+        }
+    }
+}
